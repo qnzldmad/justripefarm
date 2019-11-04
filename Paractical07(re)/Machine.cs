d7d9e38193f17cc66785cbd222b403f70d354117
Asadur Rahman Shipu <asadurrahmanshipu17@gmail.com>
2019-11-05 02:39:46 +0800

    Add new class i.e Machine

---
+++ Paractical07(re)/Machine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paractical07_re_
+{
+    class Machine
+    {
+        private int id;
+        private DateTime date;
+
+        public int Id { get => id; set => id = value; }
+        public DateTime Date { get => date; set => date = value; }
+    }
+}
